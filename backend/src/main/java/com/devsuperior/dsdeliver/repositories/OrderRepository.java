76cbfda3bb72bd7599d6a2f64842c1528e0adf62
leoasprinorocha <asprinoetecita@gmail.com>
2021-01-05 21:27:04 -0400

    commit padrão

--- backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
@@ -1,10 +1,13 @@
 package com.devsuperior.dsdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.Product;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-	
+	List<Product> findAllByOrderByNameAsc();
 }
