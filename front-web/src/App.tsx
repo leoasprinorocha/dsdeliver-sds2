3d1cd3846fae9a8b1c39a6c098a8525a7797773e
leoasprinorocha <asprinoetecita@gmail.com>
2021-01-07 22:24:23 -0400

    Add home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -4,11 +4,13 @@ import './App.css';
 import Hello from './Hello';
 import Counter from './Counter';
 import NavBar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div>
     <NavBar/>
+    <Home />
     </div>
   );
 }
