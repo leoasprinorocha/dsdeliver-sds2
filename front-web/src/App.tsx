4042d8a436d40cb61250801e59630c8f4c7610fa
leoasprinorocha <asprinoetecita@gmail.com>
2021-01-07 22:42:41 -0400

    add footer

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -5,12 +5,14 @@ import Hello from './Hello';
 import Counter from './Counter';
 import NavBar from './Navbar';
 import Home from './Home';
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
     <NavBar/>
     <Home />
+    
     </div>
   );
 }
