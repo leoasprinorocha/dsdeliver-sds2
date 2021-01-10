8ddb1dded42e89d9dcd750736ecafbb02ff2ae5a
leoasprinorocha <asprinoetecita@gmail.com>
2021-01-10 08:12:34 -0400

    Commit padrão

---
+++ front-web/src/Orders/OrderLocation.tsx
@@ -0,0 +1,17 @@
+
+function OrderLocation() {
+    return (
+        <div className="orders-location-container">
+            <div className="order-location-content">
+                <h3 className="order-location-title">
+                Selecione oinde o pedido deve ser entregue:
+                </h3>
+                <div className="filter-container"></div>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default OrderLocation;
