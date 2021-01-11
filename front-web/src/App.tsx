065e476fde9e5ef4ae72b26b7849f1d0789c7a48
leoasprinorocha <asprinoetecita@gmail.com>
2021-01-10 20:00:23 -0400

    correcao deploy

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,11 +2,6 @@
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
-import Hello from './Hello';
-import Counter from './Counter';
-import NavBar from './Navbar';
-import Home from './Home';
-import Footer from './Footer';
 import Routes from './Routes';
 
 function App() {
